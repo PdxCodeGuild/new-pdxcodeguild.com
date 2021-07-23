0aca84462a9fd3d2b818d67260a2f27c25c006bb
pjz987 <pwj2012@gmail.com>
2021-07-23 08:51:44 -0700

    updated course detail page

--- pages/courses/advanced-js.vue
+++ pages/courses/advanced-js.vue
@@ -6,13 +6,16 @@
     :course-concepts="courseConcepts"
   />
 </template>
+
 <script>
 import CourseDetail from '~/components/courses/CourseDetail.vue'
 const headerProps = {
   title: 'Advanced JavaScript Bootcamps',
-  subtitle: 'This is a test for the subtitle',
-  description: 'This is a test for the description',
+  subtitle: 'Take your skills to the next level',
+  description:
+    'Learn the MERN stack: Mongo, Express, React & Node.  Build another portfolio project and reach new coding heights.',
 }
+
 const tableProps = {
   courseDays: 'Monday - Friday',
   courseTimes: '6:00 pm - 9:36 pm',
@@ -25,10 +28,6 @@ const tableProps = {
   courseDescriptionHeader: 'Full Stack Web Development with Node and React',
   tableHeading: 'Upcoming Advanced JS Bootcamps',
 }
-tableProps.scheduledCourses.forEach((course) => {
-  course.time = '6:00 pm - 9:36 pm'
-  course.days = 'M - F'
-})
 
 const tuitionProps = {
   fullTuition: '11,500',
@@ -36,7 +35,6 @@ const tuitionProps = {
   needsBasedHigh: '8,000',
 }
 
-/* new copy */
 const courseConcepts = [
   { concept: 'Languages', areaOfStudy: 'Javascript, JSX' },
   {
@@ -55,35 +53,6 @@ const courseConcepts = [
       'Single Page Applications (SPAs), Functional Programming (closures, higher-order functions), Testing and Test-Driven Development, Agile/Scrum in a team setting (group projects), Asynchronous Event-Driven Architectures',
   },
 ]
-
-/* old copy */
-// const courseConcepts = [
-//   {
-//     concept: 'Intermediate functional programming in ES5/ES6',
-//     areaOfStudy:
-//       'Functional programming, Destructuring and rest parameters, Asynchrony, Discuss prevalent frameworks and libraries and why we chose the MERN stack',
-//   },
-//   {
-//     concept: 'Get React running using Create React App (CRA)',
-//     areaOfStudy:
-//       'Connect earlier lessons with React environment, Discuss benefits of small reusable components, Discuss state; props and rendering, Classes vs. Presentational components, Single page applications, Forms in React ',
-//   },
-//   {
-//     concept: 'Beginner server-side concepts with Node.js',
-//     areaOfStudy:
-//       'Basic request-response, client-server concepts, Application architecture, Async I/O, Abstracting and chaining callbacks ',
-//   },
-//   {
-//     concept: 'Node Express with MongoDB',
-//     areaOfStudy:
-//       'API construction, Connect our client (React) with our server (Node Express), Discuss fetch and how it works with API endpoints, Error-handling discussion with examples, Cover database concepts using Mongoose',
-//   },
-//   {
-//     concept: 'Build features towards final project',
-//     areaOfStudy:
-//       'Project management (Agile), High level web application architecture, lan, design and implement a full stack project that demonstrates an understanding of all the topics covered',
-//   },
-// ]
 
 export default {
   components: { CourseDetail },
--- pages/courses/evening.vue
+++ pages/courses/evening.vue
@@ -6,13 +6,16 @@
     :course-concepts="courseConcepts"
   />
 </template>
+
 <script>
 import CourseDetail from '~/components/courses/CourseDetail.vue'
 const headerProps = {
   title: 'Full Stack Developer Evening Bootcamps',
-  subtitle: 'This is a test for the subtitle',
-  description: 'This is a test for the description',
+  subtitle: 'Become a web developer',
+  description:
+    'Break into tech.  Learn Python, JavaScript, HTML, CSS.  Finish with a full stack capstone project built in Django.',
 }
+
 const tableProps = {
   courseDays: 'Monday - Friday',
   courseTimes: '6:00 pm - 9:36 pm',
@@ -29,10 +32,6 @@ const tableProps = {
   courseDescriptionHeader: 'Full Stack Developer Evening Bootcamps',
   tableHeading: 'Upcoming Evening Bootcamps',
 }
-// tableProps.scheduledCourses.forEach((course) => {
-//   course.time = '6:00 pm - 9:36 pm'
-//   course.days = 'M - F'
-// })
 
 const tuitionProps = {
   fullTuition: '17,600',
@@ -40,7 +39,6 @@ const tuitionProps = {
   needsBasedHigh: '10,000',
 }
 
-/* new copy */
 const courseConcepts = [
   { concept: 'Languages', areaOfStudy: 'HTML, CSS, JavaScript, Python' },
   {
@@ -59,53 +57,6 @@ const courseConcepts = [
       'Responsive Design, Database Design/ORM, Object-Oriented Programming, Debugging/Dev Tools, Capstone/Project Planning, Reading and Searching Documentation',
   },
 ]
-
-/* old copy */
-// const courseConcepts = [
-//   {
-//     concept: 'General',
-//     areaOfStudy:
-//       'Procedural programming, Object oriented programming, Test Driven Development, Debugging, Unit Testing, Functional Testing, and UI Development',
-//   },
-//   {
-//     concept: 'Python',
-//     areaOfStudy:
-//       'Python 3.6, Procedural Python, Object-oriented Python, Writing PEP8 compliant Python, The Python Standard Library, Using 3rd Party Modules',
-//   },
-//   {
-//     concept: 'JavaScript',
-//     areaOfStudy: 'Object-oriented JavaScript, DOM manipulation, ES6, jQuery',
-//   },
-//   {
-//     concept: 'Systems',
-//     areaOfStudy:
-//       'Command line (Bash and Powershell), Filesystem Management and Layout, Python Dependency Management, Virtual Environments ',
-//   },
-//   { concept: 'Frontend', areaOfStudy: 'HTML, CSS' },
-//   { concept: 'Python frameworks', areaOfStudy: 'Django 1.11' },
-//   {
-//     concept: 'Capstone',
-//     areaOfStudy: 'Your own original full-stack web application',
-//   },
-//   { concept: 'Networks', areaOfStudy: 'Http Request, Deployment Ideas ' },
-//   {
-//     concept: 'Text Editors and IDE’s',
-//     areaOfStudy: 'Atom Editor, PyCharm IDE, Visual Studio Code ',
-//   },
-//   {
-//     concept: 'Source Control',
-//     areaOfStudy: 'Using Git (Version Control), Using Github and Bitbucket ',
-//   },
-//   {
-//     concept: 'Best Practices',
-//     areaOfStudy: 'Writing documentation and good comments, Encapsulation ',
-//   },
-//   {
-//     concept: 'Problem Solving Skills',
-//     areaOfStudy:
-//       'Writing pseudo code, Finding and using documentation, Discerning what’s useful on the internet and applying it, White Boarding, The Web Developer and Inspector Tools ',
-//   },
-// ]
 
 export default {
   components: { CourseDetail },
