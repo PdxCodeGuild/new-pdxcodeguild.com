582d7d5be0be1b3b3050bc5575dfb13a48e103d3
pjz987 <pwj2012@gmail.com>
2022-06-30 12:23:46 -0700

    initial draft of new capstones page

---
+++ components/CapstoneDisplay.vue
@@ -0,0 +1,174 @@
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        gridTemplateRows: {
+          '[auto,auto,1fr]': 'auto auto 1fr',
+        },
+      },
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<template>
+  <figure class="bg-white">
+    <div>
+      <!-- Image gallery -->
+      <div
+        v-if="capstone.images"
+        class="
+          mt-6
+          max-w-2xl
+          mx-auto
+          sm:px-6
+          lg:max-w-7xl
+          lg:px-8
+          lg:grid lg:grid-cols-3
+          lg:gap-x-8
+        "
+      >
+        <div
+          class="
+            hidden
+            aspect-w-3 aspect-h-4
+            rounded-lg
+            overflow-hidden
+            lg:block
+          "
+        >
+          <img
+            :src="capstone.images[0].src"
+            :alt="capstone.images[0].alt"
+            class="w-full h-full object-center object-cover"
+          />
+        </div>
+
+        <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+          <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
+            <img
+              :src="capstone.images[1].src"
+              :alt="capstone.images[1].alt"
+              class="w-full h-full object-center object-cover"
+            />
+          </div>
+          <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
+            <img
+              :src="capstone.images[2].src"
+              :alt="capstone.images[2].alt"
+              class="w-full h-full object-center object-cover"
+            />
+          </div>
+        </div>
+        <div
+          class="
+            aspect-w-4 aspect-h-5
+            sm:rounded-lg
+            sm:overflow-hidden
+            lg:aspect-w-3 lg:aspect-h-4
+          "
+        >
+          <img
+            :src="capstone.images[3].src"
+            :alt="capstone.images[3].alt"
+            class="w-full h-full object-center object-cover"
+          />
+        </div>
+      </div>
+      <div
+        v-else
+        class="
+          mt-6
+          max-w-2xl
+          mx-auto
+          sm:px-6
+          lg:max-w-7xl
+          lg:px-8
+          aspect-w-16 aspect-h-9
+        "
+      >
+        <img :src="capstone.imageSrc" :alt="capstone.name" />
+      </div>
+      <!-- capstone info -->
+      <div
+        class="
+          max-w-2xl
+          mx-auto
+          py-10
+          px-4
+          sm:px-6
+          lg:max-w-7xl
+          lg:py-16
+          lg:px-8
+          lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr]
+          lg:gap-x-8
+        "
+      >
+        <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+          <h1
+            class="
+              text-2xl
+              font-extrabold
+              tracking-tight
+              text-gray-900
+              sm:text-3xl
+            "
+          >
+            {{ capstone.name }}
+          </h1>
+        </div>
+
+        <!-- Options -->
+        <div class="mt-4 lg:mt-0 lg:row-span-3">
+          <h2 class="sr-only">Student</h2>
+          <p class="text-3xl text-gray-900">by {{ capstone.student }}</p>
+        </div>
+
+        <div
+          class="
+            py-10
+            lg:pt-6
+            lg:pb-16
+            lg:col-start-1 lg:col-span-2
+            lg:border-r lg:border-gray-200
+            lg:pr-8
+          "
+        >
+          <!-- Description and details -->
+          <div>
+            <h3 class="sr-only">Description</h3>
+
+            <div class="space-y-6">
+              <p class="text-base text-gray-900">{{ capstone.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </figure>
+</template>
+
+<script>
+export default {
+  // props: ['capstone'], // props as array
+  props: {
+    // props as object
+    capstone: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
--- pages/capstones.vue
+++ pages/capstones.vue
@@ -1,9 +1,17 @@
 <template>
   <main>
-    <CoursesHeader
-      :mt-hood-bg="true"
-      title="Your own original full-stack web application"
+    <HeaderSimpleCentered
+      title="Capstone Projects"
+      subtitle="Your own original full-stack web application"
+      description="Placeholder for description text"
     />
+
+    <CapstoneDisplay
+      v-for="capstone in capstones"
+      :key="capstone"
+      :capstone="capstone"
+    />
+
     <CTA
       title="About Capstones"
       description="The Capstone is a web application that touches on every major technology we cover at PDX Code Guild: Python, HTML, CSS, JavaScript, and Django.
@@ -20,7 +28,40 @@
   </main>
 </template>
 <script>
+const capstones = [
+  {
+    name: 'Storied',
+    description:
+      'Storied is a place for gaming enthusiasts of well-crafted Fantasy and Sci-Fi Stories. It was built with Vue.JS for the Front End, and a Django REST Framework API for the back-end.',
+    student: 'Chris Roos',
+    imageSrc: '/images/capstones/storied/storied.png',
+  },
+  {
+    name: 'Fiber Amie',
+    description:
+      "Fiber Amie is a simple but effective knitting and crochet companion app. It's intended to be easy to use, accessible, and mobile-friendly. You can quickly look up yarn in your stash, see what needles or hooks you have, add projects and images, and use the built-in row counter to keep track of your spot in a pattern.",
+    student: 'Aimee Young',
+    imageSrc: '/images/capstones/fiber-amie/fiberamieprojectsdesktop.png',
+  },
+  {
+    name: 'Vehicle Maintenance Logbook',
+    description:
+      'A web based vehicle maintenance logbook where users can track their vehicle(s) maintenance as well as get information about their vehicle with a VIN Decoder. Can be viewed from any web connected device; providing easily accessible records of any maintenance completed by the user or a professional shop and provides general recommendations for regular maintenance intervals. A useful tool for any car owner!',
+    student: 'Mitch Chapman',
+    imageSrc: '/images/capstones/vehicle-maintenance-logbook/image.png',
+  },
+  {
+    name: 'Salieri',
+    description:
+      'Salieri is a music composition app that allows you to quickly write your own music as multi-track MIDI.  It was designed to be helpful and accessible to all kinds of users, from casual experimenters to seasoned musicians.  The ability to read sheet music or play an instrument is not required.',
+    student: 'Logan Douglass',
+    imageSrc: '/images/capstones/salieri/salfix.png',
+  },
+]
 export default {
+  data() {
+    return { capstones }
+  },
   head: {
     title: 'PDX Code Guild | Capstones',
     meta: [
